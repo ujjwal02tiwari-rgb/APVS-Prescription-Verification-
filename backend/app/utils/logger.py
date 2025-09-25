cccf76b07b4c5b1186a10d6ee757127db0c41a16
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-25 19:26:09 +0530

    Create logger.py

---
+++ backend/app/utils/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+logger = logging.getLogger("pharmbot")
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
